2910a035c1b1d90e34da6f1c9930ff8b3b763efb
W.A.R.J <frivandres2011@hotmail.com>
2025-05-08 22:23:35 -0500

    WR: First commit.

---
+++ dj_store/settings/dev.py
@@ -0,0 +1,23 @@
+# py
+# django
+from dj_store.settings.base import *
+# third
+from decouple import config
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY_DEV")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG_DEV", default=True, cast=bool)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS_DEV", default=['127.0.0.1', 'localhost'], cast=lambda v: [host.strip() for host in v.split(',')])
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': config("DB_ENGINE_DEV"),
+        'NAME': BASE_DIR / config("DB_NAME_DEV"),
+    }
+}
